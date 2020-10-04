c8403833fbbdad4e7e80b73b75a46ba28a680aca
HIAGO_NATAN_FERNANDES_DE_SOUSA <hiago.sousa@ccc.ufcg.edu.br>
2020-10-03 22:04:21 -0300

    delete user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,16 @@
-const UserService = require('../services/user.service');
-const log = require('../services/log.service');
-const http = require('../services/http.service');
+const UserService = require("../services/user.service");
+const log = require("../services/log.service");
+const http = require("../services/http.service");
 
-const create = async (req,res) => {
-    try {
-        log.info("Iniciando criação de usuário");
+const create = async (req, res) => {
+  try {
+    log.info("Iniciando criação de usuário");
 
-        const data = req.body;
-        const email = data.email;
-        const password = data.password;
-        /* Dá erro aqui, investigar
+    const data = req.body;
+    const email = data.email;
+    const password = data.password;
+    /* Dá erro aqui, investigar
+        dá pra usar o unique do mongoose e capturar o erro específico.
         const user = await UserService.findByEmail(email)
                     .then(userData => {
                         if(userData){
@@ -18,53 +19,68 @@ const create = async (req,res) => {
                     })
 */
 
-        const createdUser = await UserService.create(data)
-                            .then(userData => {
-                                return http.ok(res,userData);
-                            });
-        
-        return createdUser;
+    const createdUser = await UserService.create(data);
+    return http.ok(res, createdUser);
+  } catch (error) {
+    log.error("Erro cadastrar usuário", req.originalUrl, error);
+    return http.internalServerError(res);
+  }
+};
 
-    } catch (error) {
-        log.error("Erro cadastrar usuário", req.originalUrl, error);
-        return http.internalServerError(res);
+const get = async (req, res) => {
+  try {
+    log.info("Iniciando obtenção de usuário");
+
+    const id = req.params.id;
+
+    const user = await UserService.get(id);
+
+    if (!user) {
+      return http.notFound(res, "Usuário não encontrado");
     }
-}
 
-const get = async (req,res) => {
-    try {
-        log.info("Iniciando obtenção de usuário");
+    return http.ok(res, user);
+  } catch (error) {
+    log.error("Erro obter usuário", req.originalUrl, error);
+    http.internalServerError(res);
+  }
+};
 
-        const id = req.params.id;
+const getAll = async (req, res) => {
+  try {
+    log.info("Iniciando obtenção de todos os usuários");
 
-        const user = await UserService.get(id);
+    const user = await UserService.getAll();
 
-        if(!user){
-            return http.notFound(res, "Usuário não encontrado");
-        }
+    return http.ok(res, user);
+  } catch (error) {
+    log.error("Erro obter usuários", req.originalUrl, error);
+    http.internalServerError(res);
+  }
+};
 
-        return http.ok(res, user);
-    } catch(error){
-        log.error("Erro obter usuário", req.originalUrl, error);
-        http.internalServerError(res);
-    }
-}
+const deleteUser = async (req, res) => {
+  try {
+    log.info("Deletando um usuario do sistema");
 
-const getAll = async (req,res) => {
-    try {
-        log.info("Iniciando obtenção de todos os usuários");
+    const id = req.params.id;
 
-        const user = await UserService.getAll();
-
-        return http.ok(res, user);
-    } catch(error){
-        log.error("Erro obter usuários", req.originalUrl, error);
-        http.internalServerError(res);
-    }
-}
+    const user = await UserService.remove(id).then((userData) => {
+      if (!userData) {
+        return http.notFound(res, "Nenhum usuário encontrado");
+      }
+      return http.ok(res, userData);
+    });
+    return user;
+  } catch (error) {
+    log.error("Erro ao tentar deletar usuario!", res.originalUrl, error);
+    http.internalServerError(res);
+  }
+};
 
 module.exports = {
-    create,
-    get,
-    getAll
-}
+  create,
+  get,
+  getAll,
+  deleteUser,
+};
