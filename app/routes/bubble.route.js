776f2dfbfb464e26dcc464ad5b0d46b1bc746b96
HIAGO_NATAN_FERNANDES_DE_SOUSA <hiago.sousa@ccc.ufcg.edu.br>
2020-10-06 23:06:14 -0300

    add router in bubble

--- app/routes/bubble.route.js
+++ app/routes/bubble.route.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const controller = require('../controllers/bubble.controller');
 
-
 router.post('/', controller.create);
+router.get('/', controller.get);
+router.delete('/', controller.deleteBubble);
+router.update('/', controller.update);
 
 module.exports = router;
